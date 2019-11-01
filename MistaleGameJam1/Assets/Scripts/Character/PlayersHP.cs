69a8e637db7fd606c68cfe0aa95413a1f10bb8a5
MariEss <46387164+MariEss@users.noreply.github.com>
2019-11-01 16:40:59 +0100

    PlayersHP script pour gestion de la vie

--- MistaleGameJam1/Assets/Scripts/Character/PlayersHP.cs
+++ MistaleGameJam1/Assets/Scripts/Character/PlayersHP.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class PlayersHP : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem explosionParticles;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    IEnumerator Death()
+    {
+        yield return new WaitForSeconds(.1f);
+        Destroy(gameObject);
+    }
+
+    private void uDed()
+    {
+        explosionParticles.Play();
+        StartCoroutine(Death());
     }
 
     // Update is called once per frame
