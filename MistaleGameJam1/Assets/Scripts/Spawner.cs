ae6e31330c8a35d94b1e0381eb57a9e592be1b6f
MariEss <46387164+MariEss@users.noreply.github.com>
2019-11-01 23:32:18 +0100

    Menu gameover proto fait

---
+++ MistaleGameJam1/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> obstacles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(SpawningManager());
+    }
+
+    IEnumerator SpawningManager()
+    {
+        yield return new WaitForSeconds(1f);
+        Instantiate(obstacles[Random.Range(0,obstacles.Count)], transform.position, Quaternion.identity);
+        StartCoroutine(SpawningManager());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
