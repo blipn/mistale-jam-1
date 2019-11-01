942133c5bee63740612b0d43be4e13b1792efd0e
unknown <oblipn@gmail.com>
2019-11-01 15:23:28 +0100

    Ajout du menu + game manager

---
+++ MistaleGameJam1/Assets/Scenes/Quentin/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    private static bool isPaused = true;
+    [SerializeField] private GameObject pauseMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = 1f;
+    }
+    
+    void Pause() {
+        isPaused = true;
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = 0f;
+    }
+
+    static void GameOver()
+    {
+        
+    }
+    
+}
