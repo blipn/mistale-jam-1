30781411825ee5e78d1469202df6e8452488cdb5
biblion84 <biblion84@free.fr>
2019-11-01 16:12:04 +0100

    merge conflict de Obstacle OK

--- MistaleGameJam1/Assets/Scripts/Obstacle.cs
+++ MistaleGameJam1/Assets/Scripts/Obstacle.cs
@@ -8,31 +8,23 @@ public class Obstacle : MonoBehaviour
     [SerializeField] ParticleSystem explosionParticles;
     private bool hasPlayed;
     public int life = 2;
-<<<<<<< HEAD
+
     public float intensityShake = 1f;
     public float durationShake = 0.1f;
+
     public void Hit()
     {
         life -= 1;
         CameraManager.Instance.CameraShake.Shake(durationShake, intensityShake);
-=======
-    
+    }
+
     IEnumerator uDed()
     {
         yield return new WaitForSeconds(.1f);
         Destroy(gameObject);
     }
 
-    public void Hit()
-    {
-        life -= 1;
-        //CameraManager.Instance.CameraShake.Shake();
->>>>>>> 9fd1bc0ea877056604284909ce3fcbe7fbdb750d
-        if (life <= 0)
-        {
-            Die();
-        }
-    }
+
 
     public void Die()
     {
