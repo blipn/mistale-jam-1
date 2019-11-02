656f3042a10163550f541d8bdbae4a229ede1321
Paladev <paladev.freelance@gmail.com>
2019-11-02 12:11:57 +0100

    Kill player when attack object type BorderCamera (PasCassableKiller ou Camera qui l'attrape)

--- MistaleGameJam1/Assets/Scripts/Character/PlayersHP.cs
+++ MistaleGameJam1/Assets/Scripts/Character/PlayersHP.cs
@@ -33,9 +33,18 @@ public class PlayersHP : MonoBehaviour
         StartCoroutine(Death());
         GameManager.Instance.GameOver();
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if ((collision.gameObject.tag == "Enemy" && !animator.GetBool("IsAttacking")) || collision.gameObject.tag == "BorderCamera")
+        {
+            uDed();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "BorderCamera")
         {
             uDed();
         }
