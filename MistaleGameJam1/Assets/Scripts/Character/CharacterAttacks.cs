638866c2acc3a7a3c4ca86f53e32046c434d69c0
Paladev <paladev.freelance@gmail.com>
2019-11-02 13:10:51 +0100

    Implement first sound
    First sounds intro for prg and runner
    First sound character for attacks : 3 type of sounds attacks.
    TODO : other sounds character

--- MistaleGameJam1/Assets/Scripts/Character/CharacterAttacks.cs
+++ MistaleGameJam1/Assets/Scripts/Character/CharacterAttacks.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CharacterAttacks : MonoBehaviour
 {
+    public AudioSource attackSoundVoid;
+    public AudioSource attackSoundEnemy;
+    public AudioSource attackSoundObstacle;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Obstacle")
         {
+            attackSoundVoid.mute = true;
+            attackSoundObstacle.Play();
             collision.GetComponent<Obstacle>().Hit();
         }
-
-        if(collision.tag == "Enemy")
+        else if(collision.tag == "Enemy")
         {
+            attackSoundVoid.mute = true;
+            attackSoundEnemy.Play();
             collision.GetComponent<EnemyBehavior>().Hit();
         }
     }
