eefb7aa7d7069daa29ffe31174d83a2ec1c8fad7
Idof <iduffybuck@gmail.com>
2019-11-09 17:07:55 +0100

    Tout les prefabs sont maintenant en LWRP, les particules aussi

---
+++ MistaleGameJam1/Assets/Scripts/RemoveRunTitle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveRunTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(DestroyThis), 5);
+    }
+
+    private void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+}
